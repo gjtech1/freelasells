0a1cc6cf05a3c84c7cecd0bd30626c1dfb4e4ca9
gjtech1 <168377791+gjtech1@users.noreply.github.com>
2024-04-29 19:48:03 -0300

    scripts.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const translations = {
-    "en": {
-        "searchPlaceholder": "Search downloads...",
-        "searchButton": "Search"
-    },
-    "es": {
-        "searchPlaceholder": "Buscar descargas...",
-        "searchButton": "Buscar"
-    },
-    "tr": {
-        "searchPlaceholder": "İndirmeleri ara...",
-        "searchButton": "Ara"
-    },
-    "hi": {
-        "searchPlaceholder": "डाउनलोड खोजें...",
-        "searchButton": "खोजें"
-    },
-    "fr": {
-        "searchPlaceholder": "Rechercher des téléchargements...",
-        "searchButton": "Rechercher"
-    },
-    "pt-BR": {
-        "searchPlaceholder": "Buscar downloads...",
-        "searchButton": "Buscar"
-    }
-};
-
 document.addEventListener('DOMContentLoaded', function() {
     const language = navigator.language.split('-')[0]; // Obter apenas a parte do idioma (ex: "en" de "en-US")
     const translation = translations[language] || translations['en']; // Usar inglês como padrão se não houver tradução disponível
